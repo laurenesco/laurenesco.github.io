d601148dde0f7bf0a2bd0bc08077394f2343243f
lescobedo4@mail.stmarytx.edu <89123282+laurenesco@users.noreply.github.com>
2023-06-09 15:38:11 -0500

    Table is now sortable and has alternating row colors

---
+++ sortable-tables.js
@@ -0,0 +1,78 @@
+// Function to sort the table data
+function sortTable(event) {
+    const table = event.target.closest("table"); // Get the parent table element
+    const tbody = table.querySelector("tbody"); // Get the table body
+    const rows = Array.from(tbody.querySelectorAll("tr")); // Get all table rows
+  
+    // Get the column index to sort
+    const column = Array.from(event.target.parentNode.children).indexOf(event.target);
+  
+    // Get the current sorting order of the column
+    const currentSortOrder = event.target.dataset.sortOrder || "asc";
+    const sortOrder = currentSortOrder === "asc" ? "desc" : "asc";
+    event.target.dataset.sortOrder = sortOrder;
+  
+    // Sort the rows based on the content of the selected column
+    rows.sort((a, b) => {
+      const cellA = a.querySelectorAll("td")[column].textContent.trim();
+      const cellB = b.querySelectorAll("td")[column].textContent.trim();
+      let result;
+  
+      // Apply different sorting based on the sorting style specified in the class of the header
+      if (event.target.classList.contains("sortable-date")) {
+        const dateA = new Date(cellA);
+        const dateB = new Date(cellB);
+        result = dateA - dateB;
+      } else if (event.target.classList.contains("sortable-numeric")) {
+        const numberA = parseFloat(cellA);
+        const numberB = parseFloat(cellB);
+        result = numberA - numberB;
+      } else {
+        result = cellA.localeCompare(cellB, undefined, { numeric: true, sensitivity: 'base' });
+      }
+  
+      return sortOrder === "asc" ? result : -result;
+    });
+  
+    // Remove existing rows from the table
+    rows.forEach(row => tbody.removeChild(row));
+  
+    // Append the sorted rows back to the table
+    rows.forEach(row => tbody.appendChild(row));
+
+  // Apply alternating row colors
+  applyAlternatingRowColors(table);
+  }
+
+// Function to apply alternating row colors
+function applyAlternatingRowColors(table) {
+    const rows = table.querySelectorAll("tbody tr");
+    const alternateClass = "alternate";
+  
+    // Remove existing row colors
+    rows.forEach(row => row.classList.remove(alternateClass));
+  
+    // Add alternating row colors
+    rows.forEach((row, index) => {
+      if (index % 2 === 0) {
+        row.classList.add(alternateClass);
+      }
+    });
+  }
+  
+  
+  // Add click event listener to all sortable tables
+  const sortableTables = document.querySelectorAll(".sortable");
+  
+  sortableTables.forEach(table => {
+    const headers = table.querySelectorAll("th");
+  
+    // Add click event listener to each table header
+    headers.forEach(header => {
+      header.addEventListener("click", sortTable);
+      header.style.cursor = "pointer"; // Change cursor to indicate sortable header
+    });
+
+      // Apply initial alternating row colors
+        applyAlternatingRowColors(table); 
+  });
