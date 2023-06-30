8c100e82798f8a544a837a9cbc99f35ff4d626e0
lescobedo4@mail.stmarytx.edu <89123282+laurenesco@users.noreply.github.com>
2023-06-30 00:12:50 -0500

    Refactoring for readability

--- sortable-tables.js
+++ sortable-tables.js
@@ -1,24 +1,30 @@
 // Function to sort the table data
 function sortTable(event) {
+  // Retrieve table elements
   const table = event.target.closest("table");
   const tbody = table.querySelector("tbody");
-  const rows = Array.from(tbody.querySelectorAll("tr")); // Get all table rows
+  const rows = Array.from(tbody.querySelectorAll("tr"));
 
-  // Get the column index to sort
-  const column = Array.from(event.target.parentNode.children).indexOf(event.target);
+  // Parse column indices
+  const classPrefix = "sortable-onload";
+  const classList = table.className.split(" ");
+  const sortableClass = classList.find(className => className.startsWith(classPrefix));
+  const columnIndices = sortableClass.replace(classPrefix, "")
+                                     .split("-")
+                                     .filter(index => index !== "")
+                                     .map(index => parseInt(index));
 
-  // Get the current sorting order of the column
+  // Determine the sorting order of the column
   const currentSortOrder = event.target.dataset.sortOrder || "desc";
   const sortOrder = currentSortOrder === "asc" ? "desc" : "asc";
   event.target.dataset.sortOrder = sortOrder;
 
   // Sort the rows based on the content of the selected column
   rows.sort((a, b) => {
-    const cellA = a.querySelectorAll("td")[column].textContent.trim();
-    const cellB = b.querySelectorAll("td")[column].textContent.trim();
+    const cellA = a.querySelectorAll("td")[columnIndices[0]].textContent.trim();
+    const cellB = b.querySelectorAll("td")[columnIndices[0]].textContent.trim();
     let result;
 
-    // Apply different sorting based on the sorting style specified in the class of the header
     if (event.target.classList.contains("sortable-date")) { // Date sort
       const dateA = new Date(cellA);
       const dateB = new Date(cellB);
@@ -28,21 +34,34 @@ function sortTable(event) {
       const numberB = parseFloat(cellB);
       result = numberA - numberB;
     } else {
-      result = cellA.localeCompare(cellB, undefined, { numeric: true, sensitivity: 'base' }); // Alphanumeric sort
+      result = cellA.localeCompare(cellB, undefined, { numeric: true, sensitivity: 'base' }); // Alphanumeric sort [Default]
+    }
+
+    // Sort by secondary column
+    if (result == 0 && columnIndices.length >= 2) {
+      const secondColumn = columnIndices[1];
+      const secondCellA = a.querySelectorAll("td")[secondColumn].textContent.trim();
+      const secondCellB = b.querySelectorAll("td")[secondColumn].textContent.trim();
+
+      if (event.target.classList.contains("sortable-date")) { // Date sort
+        const dateA = new Date(secondCellA);
+        const dateB = new Date(secondCellB);
+        result = dateB - dateA;
+      } else if (event.target.classList.contains("sortable-numeric")) { // Numeric sort
+        const numberA = parseFloat(secondCellA);
+        const numberB = parseFloat(secondCellB);
+        result = numberA - numberB;
+      } else {
+        result = secondCellA.localeCompare(secondCellB, undefined, { numeric: true, sensitivity: 'base' }); // Alphanumeric sort
+      }
     }
 
     return sortOrder === "asc" ? result : -result;
   });
 
-  // Remove existing arrow
+  // Apply sort direction arrow
   const headers = table.querySelectorAll("th");
-  headers.forEach(header => {
-    header.textContent = header.textContent.replace(/\u2191|\u2193/g, '').trim();
-  });
-
-  // Add new arrow 
-  const sortOrderArrow = sortOrder === "asc" ? " \u2191" : " \u2193";
-  event.target.textContent = event.target.textContent.trim() + sortOrderArrow;
+  applySortArrow(headers, sortOrder, event.target);
 
   // Remove and reappend rows in the table
   rows.forEach(row => tbody.removeChild(row));
@@ -50,6 +69,17 @@ function sortTable(event) {
 
   // Apply alternating row colors
   applyAlternatingRowColors(table);
+}
+
+// Function to apply sort direction arrow to headers
+function applySortArrow(headers, sortOrder, eventTarget) {
+  headers.forEach(header => {
+    header.textContent = header.textContent.replace(/\u2191|\u2193/g, '').trim();
+  });
+
+  // Add new arrow 
+  const sortOrderArrow = sortOrder === "asc" ? " \u2191" : " \u2193";
+  eventTarget.textContent = eventTarget.textContent.trim() + sortOrderArrow;
 }
 
 // Function to apply alternating row colors
@@ -69,10 +99,15 @@ function applyAlternatingRowColors(table) {
   }
 }
 
+//
+//  Start script
+//
+
 // Find tables with class "sortable-onload-N" and sort by the specified column on load
 const sortableTables = document.querySelectorAll("table[class*='sortable-onload-']");
 
 sortableTables.forEach(table => {
+  // Retrieve table elements
   const headers = table.querySelectorAll("th");
 
   // Isolate the correct class, necessary for tables with multiple classes
@@ -81,10 +116,6 @@ sortableTables.forEach(table => {
   const sortableClass = classList.find(className => className.startsWith(classPrefix));
 
   if (sortableClass) {
-    // Get the column index from the class name
-    const classIndex = sortableClass.indexOf(classPrefix) + classPrefix.length;
-    const column = Math.abs(parseInt(table.className.substring(classIndex), 10)); // Must take absolute value due to preceeding "-" in class name!
-
     // Add click event listener to each table header
     headers.forEach(header => {
       header.addEventListener("click", sortTable);
@@ -94,14 +125,16 @@ sortableTables.forEach(table => {
         header.style.color = "#006699";
       }
     });
-//debugger;
-    // Trigger the sorting based on the column index
-    const headerToSort = headers[column];
-    if (headerToSort) {
-      // Set the initial sorting order to "asc" for the header to be sorted on load
-      headerToSort.dataset.sortOrder = table.classList.contains("descending") ? "asc" : "desc";
-      sortTable({ target: headerToSort }); // Manually trigger the sorting function
-    }
+
+    // Trigger the sorting based on the column indices
+    columnIndices.forEach(index => {
+      const headerToSort = headers[index];
+      if (headerToSort) {
+        // Set the initial sorting order to "asc" for the headers to be sorted on load
+        headerToSort.dataset.sortOrder = table.classList.contains("descending") ? "asc" : "desc";
+        sortTable({ target: headerToSort }); // Manually trigger the sorting function
+      }
+    });
   }
 
   // Apply initial alternating row colors
